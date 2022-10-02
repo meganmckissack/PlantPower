f26da01c0c714ff6d7c0a9dd1eb8950c182043d5
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-02 16:45:22 -0700

    add files to merge branches

--- src/index.js
+++ src/index.js
@@ -1,107 +1,124 @@
-import { blueFood, addPlant, plantsState, storeState, feed, hydrate, superWater, stateControl, stateControl2} from '../src/plant.js';
+import { blueFood, addPlant, plantsState, storeState, feed, hydrate, superWater, stateControl, stateControl2, storeListState, changeListState} from '../src/plant.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-// //UI Logic
+//UI Logic
 // function (plantNum){
 //   export const addPlantStateControl = changeState(`plant${plantNum}`)()
 // }
 
-function incrementPlants(){
-  const newState = plantsState(addPlant);
-  console.log(newState);
-  const plantNumber = newState.plantCounter;
-  addPlantHtml(plantNumber);
-  // addPlantScripts(plantNumber);
-}
 
-function addPlantHtml(num){
-  $(".container").append(`<div class="plant-container">
+function addPlantHtml(plantNum){
+  $(".container").append(`<div class="plant-container" id="plant-container-${plantNum}">
+  <h1>Plant #${plantNum}:</h1>
   <div class="grow-buttons">
-    <button class="btn-success" id="feed${num}">Add soil</button>
-    <button class="btn-success" id="superFeed${num}">give your plant the blue food!</button>
-    <button class="btn-success" id="water${num}">water your baby</button>
-    <button class="btn-success" id="superWater${num}">Give them a super water!!</button>
-    <button class="btn-success" id="show-state${num}">Current Stats</button>
+    <button class="btn-success feed" id="feed${plantNum}">Add soil</button>
+    <button class="btn-success superFeed" id="superFeed${plantNum}">give your plant the blue food!</button>
+    <button class="btn-success water" id="water${plantNum}">water your baby</button>
+    <button class="btn-success superWater" id="superWater${plantNum}">Give them a super water!!</button>
+    <button class="btn-success show-state" id="show-state${plantNum}">Current Stats</button>
   </div>
   <div class="plant-stats">
-    <h1>Your NEW Plant's Stats:</h1>
-    <h3><div id="soil-value-${num}">0</div></h3>
-    <h3><div id="water-value-${num}">0</div></h3>
+    <h2>Stats:</h2>
+    <h3><div id="soil-value-${plantNum}">0</div></h3>
+    <h3><div id="water-value-${plantNum}">0</div></h3>
   </div>
+  <p>--------------------------------------</p>
 </div>`);
+$('.plant-container').on("click", clickHandler);
 }
 
 // function addPlantScripts(num){
 //   const newState = 
 // }
 
+function clickHandler(event){
+  event.preventDefault();
+  console.log(event);
+  console.log("hello");
+}
+
 // //JQUERY UI Logic
 $(document).ready(function() {
   console.log("docReady");
 
+  const listControl = storeListState();
+
   // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
   $('#addPlant').click(function() {
-    incrementPlants();
+    const stateControl = storeState();
+    const addPlant = changeListState(stateControl);
+    console.log(listControl);
+    const newList = listControl(addPlant);
+    const plantNumber = newList.length - 1;
+    addPlantHtml(plantNumber);
+    // addPlantScripts(plantNumber);
   });
+
+  // $('.plant-container').click(function(event) {
+  //   console.log(event);
+  // });
+
+  $('.grow-buttons').on("click", clickHandler);
   
-  $('#feed').click(function() {
-    const newState = stateControl(feed);
-    $('#soil-value').text(`Soil: ${newState.soil}`);
+  // $('.feed').click(function(event) {
+  //   console.log(event);
+  //   const newState = stateControl(feed);
+  //   $('#soil-value').text(`Soil: ${newState.soil}`);
     
-  });
+  // });
 
-  $('#superFeed').click(function() {
-    const newState = stateControl(blueFood);
-    $('#soil-value').text(`Soil: ${newState.soil}`);
-  });
+  // $('#superFeed').click(function() {
+  //   const newState = stateControl(blueFood);
+  //   $('#soil-value').text(`Soil: ${newState.soil}`);
+  // });
 
-  $('#water').click(function() {
-    const newState = stateControl(hydrate);
-    $('#water-value').text(`Water: ${newState.water}`);
-  });
+  // $('#water').click(function() {
+  //   const newState = stateControl(hydrate);
+  //   $('#water-value').text(`Water: ${newState.water}`);
+  // });
 
-  $('#superWater').click(function() {
-    const newState = stateControl(superWater);
-    $('#water-value').text(`Water: ${newState.water}`);
-  });
+  // $('#superWater').click(function() {
+  //   const newState = stateControl(superWater);
+  //   $('#water-value').text(`Water: ${newState.water}`);
+  // });
   
-  // This function doesn't actually do anything useful in this application — it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  // // This function doesn't actually do anything useful in this application — it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
   
-  $('#show-state').click(function() {
-    // We just need to call stateControl() without arguments to see our current state.
-    const currentState = stateControl();
-    $('#soil-value').text(`Soil: ${currentState.soil}`);
-    console.log(`Soil: ${currentState.soil}`);
-  });
+  // $('#show-state').click(function() {
+  //   // We just need to call stateControl() without arguments to see our current state.
+  //   const currentState = stateControl();
+  //   $('#soil-value').text(`Soil: ${currentState.soil}`);
+  //   console.log(`Soil: ${currentState.soil}`);
+  // });
 
-  //PLANT 2:
-  $('#feed2').click(function() {
-    const newState = stateControl2(feed);
-    $('#soil-value-2').text(`Soil: ${newState.soil}`);
-  });
+  // //PLANT 2:
+  // $('#feed2').click(function() {
+  //   const newState = stateControl2(feed);
+  //   $('#soil-value-2').text(`Soil: ${newState.soil}`);
+  // });
 
-  $('#superFeed2').click(function() {
-    const newState = stateControl2(blueFood);
-    $('#soil-value-2').text(`Soil: ${newState.soil}`);
-  });
+  // $('#superFeed2').click(function() {
+  //   const newState = stateControl2(blueFood);
+  //   $('#soil-value-2').text(`Soil: ${newState.soil}`);
+  // });
 
-  $('#water2').click(function() {
-    const newState = stateControl2(hydrate);
-    $('#water-value-2').text(`Water: ${newState.water}`);
-  });
+  // $('#water2').click(function() {
+  //   const newState = stateControl2(hydrate);
+  //   $('#water-value-2').text(`Water: ${newState.water}`);
+  // });
 
-  $('#superWater2').click(function() {
-    const newState = stateControl2(superWater);
-    $('#water-value-2').text(`Water: ${newState.water}`);
-  });
+  // $('#superWater2').click(function() {
+  //   const newState = stateControl2(superWater);
+  //   $('#water-value-2').text(`Water: ${newState.water}`);
+  // });
 
-  $('#show-state2').click(function() {
-    const currentState = stateControl2();
-    $('#soil-value-2').text(`Soil: ${currentState.soil}`);
-    $('#water-value-2').text(`Soil: ${currentState.water}`);
-    console.log(`Soil: ${currentState.soil}, Water: ${currentState.water}`);
-  });
+  // $('#show-state2').click(function() {
+  //   const currentState = stateControl2();
+  //   $('#soil-value-2').text(`Soil: ${currentState.soil}`);
+  //   $('#water-value-2').text(`Soil: ${currentState.water}`);
+  //   console.log(`Soil: ${currentState.soil}, Water: ${currentState.water}`);
+  // });
 
 });
--- src/plant.js
+++ src/plant.js
@@ -8,9 +8,9 @@ export const storeState = () => {
   };
 };
 
-export const stateControl = storeState();
-export const stateControl2 = storeState();
-export const plantsState = storeState();
+// export const stateControl = storeState();
+// export const stateControl2 = storeState();
+// export const plantsState = storeState();
 
 // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
 
@@ -30,6 +30,23 @@ export const blueFood = changeState("soil")(5);
 export const hydrate = changeState("water")(1);
 export const superWater = changeState("water")(5);
 
-export const addPlant = changeState("plantCounter")(1);
+//stores list of plants as they are created
+
+export const storeListState = () => {
+  let currentState = [];
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = [...newState];
+    return newState;
+  }
+}
+
+export const changeListState = (plant) => {
+  return (state) => ([
+    ...state,
+    plant
+  ])
+}
+
 
 
