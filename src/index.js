db7e2320ca72e6f7ddb10b7a9cb5926b54e2ebca
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-03 20:22:20 -0700

    refactor to add event listeners and update state for new plants
    Co-authored-by: Megan McKissack <meganmckissack@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './css/styles.css';
 function addPlantHtml(plantNum){
   $(".container").append(`<div class="plant-container" id="plant-container-${plantNum}">
   <h1>Plant #${plantNum}:</h1>
-  <div class="grow-buttons">
+  <div class="grow-buttons" id="grow-buttons-${plantNum}">
     <button class="btn-success feed" id="feed${plantNum}">Add soil</button>
     <button class="btn-success superFeed" id="superFeed${plantNum}">give your plant the blue food!</button>
     <button class="btn-success water" id="water${plantNum}">water your baby</button>
@@ -24,101 +24,51 @@ function addPlantHtml(plantNum){
     <h3><div id="soil-value-${plantNum}">0</div></h3>
     <h3><div id="water-value-${plantNum}">0</div></h3>
   </div>
-  <p>--------------------------------------</p>
-</div>`);
-$('.plant-container').on("click", clickHandler);
+    <p>--------------------------------------</p>
+  </div>`);
+  $(`#grow-buttons-${plantNum}`).on("click", buttonHandler);
 }
 
 // function addPlantScripts(num){
 //   const newState = 
 // }
 
-function clickHandler(event){
+function buttonHandler(event){
   event.preventDefault();
-  console.log(event);
-  console.log("hello");
+  const id = event.target.id.replace( /^\D+/g, '');
+  console.log(id);
+  if (event.target.id.includes("feed")){
+    const stateControl = listControl()[id];
+    const newState = stateControl(feed);
+    console.log(newState);
+    console.log(listControl());
+    $(`#soil-value-${id}`).text(`Soil: ${newState.soil}`);
+  } else if (event.target.id.includes("Feed")){
+    const stateControl = listControl()[id];
+    const newState = stateControl(blueFood);
+    $(`#soil-value-${id}`).text(`Soil: ${newState.soil}`);
+  } else if (event.target.id.includes("water")){
+    const stateControl = listControl()[id];
+    const newState = stateControl(hydrate);
+    $(`#water-value-${id}`).text(`Water: ${newState.water}`);
+  }  else if (event.target.id.includes("Water")){
+    const stateControl = listControl()[id];
+    const newState = stateControl(superWater);
+    $(`#water-value-${id}`).text(`Water: ${newState.water}`);
+  }
 }
+
+const listControl = storeListState();
 
 // //JQUERY UI Logic
 $(document).ready(function() {
   console.log("docReady");
 
-  const listControl = storeListState();
-
-  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
   $('#addPlant').click(function() {
     const stateControl = storeState();
     const addPlant = changeListState(stateControl);
-    console.log(listControl);
     const newList = listControl(addPlant);
     const plantNumber = newList.length - 1;
     addPlantHtml(plantNumber);
-    // addPlantScripts(plantNumber);
   });
-
-  // $('.plant-container').click(function(event) {
-  //   console.log(event);
-  // });
-
-  $('.grow-buttons').on("click", clickHandler);
-  
-  // $('.feed').click(function(event) {
-  //   console.log(event);
-  //   const newState = stateControl(feed);
-  //   $('#soil-value').text(`Soil: ${newState.soil}`);
-    
-  // });
-
-  // $('#superFeed').click(function() {
-  //   const newState = stateControl(blueFood);
-  //   $('#soil-value').text(`Soil: ${newState.soil}`);
-  // });
-
-  // $('#water').click(function() {
-  //   const newState = stateControl(hydrate);
-  //   $('#water-value').text(`Water: ${newState.water}`);
-  // });
-
-  // $('#superWater').click(function() {
-  //   const newState = stateControl(superWater);
-  //   $('#water-value').text(`Water: ${newState.water}`);
-  // });
-  
-  // // This function doesn't actually do anything useful in this application — it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
-  
-  // $('#show-state').click(function() {
-  //   // We just need to call stateControl() without arguments to see our current state.
-  //   const currentState = stateControl();
-  //   $('#soil-value').text(`Soil: ${currentState.soil}`);
-  //   console.log(`Soil: ${currentState.soil}`);
-  // });
-
-  // //PLANT 2:
-  // $('#feed2').click(function() {
-  //   const newState = stateControl2(feed);
-  //   $('#soil-value-2').text(`Soil: ${newState.soil}`);
-  // });
-
-  // $('#superFeed2').click(function() {
-  //   const newState = stateControl2(blueFood);
-  //   $('#soil-value-2').text(`Soil: ${newState.soil}`);
-  // });
-
-  // $('#water2').click(function() {
-  //   const newState = stateControl2(hydrate);
-  //   $('#water-value-2').text(`Water: ${newState.water}`);
-  // });
-
-  // $('#superWater2').click(function() {
-  //   const newState = stateControl2(superWater);
-  //   $('#water-value-2').text(`Water: ${newState.water}`);
-  // });
-
-  // $('#show-state2').click(function() {
-  //   const currentState = stateControl2();
-  //   $('#soil-value-2').text(`Soil: ${currentState.soil}`);
-  //   $('#water-value-2').text(`Soil: ${currentState.water}`);
-  //   console.log(`Soil: ${currentState.soil}, Water: ${currentState.water}`);
-  // });
-
 });
